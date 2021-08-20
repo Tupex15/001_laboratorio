f2e071e7792685c8854ca628b137ea37f055b44c
Rafael <rafa_sena@msn.com>
2021-08-20 09:14:07 -0300

    main2

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>001_laboratorio</title>
-    <link rel="stylesheet" href="css/estilo.css" type="text/css"/>
-</head>
-<body>
-    <div class="central">
-        <div class="cabecalho">
-            <img src="#" alt="aqui terá uma img"/>
-        </div>
-        <div class="principal">
+<?php 
+    include 'layout/cabecalho.php';
+?>
             <div class="lista-empresas">
                 <h3> Escolha para qual empresa do grupo deseja enviar seu curriculo:</h3>
                 <ul>
@@ -20,7 +8,6 @@
                     <li><a href="curriculo.php">LAB 001</a></li>
                 </ul>
             </div>
-        </div> <!-- fim principal -->
-    </div> <!-- fim central -->
-</body>
-</html>
+<?php 
+    include 'layout/rodape.php';
+?>
