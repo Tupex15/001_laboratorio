564c8a0f4f847b5106c4d48a1efc02dbd9901fc3
Rafael <rafa_sena@msn.com>
2021-08-20 08:47:17 -0300

    main

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
-<?php 
-
-echo "Olá mundo!";
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>001_laboratorio</title>
+    <link rel="stylesheet" href="css/estilo.css" type="text/css"/>
+</head>
+<body>
+    <div class="central">
+        <div class="cabecalho">
+            <img src="#" alt="aqui terá uma img"/>
+        </div>
+        <div class="principal">
+            <div class="lista-empresas">
+                <h3> Escolha para qual empresa do grupo deseja enviar seu curriculo:</h3>
+                <ul>
+                    <li><a href="curriculo.php">LAB 000</a></li>
+                    <li><a href="curriculo.php">LAB 001</a></li>
+                </ul>
+            </div>
+        </div> <!-- fim principal -->
+    </div> <!-- fim central -->
+</body>
+</html>
