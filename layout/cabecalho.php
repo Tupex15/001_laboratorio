4c8f1bd8777cea768fa0ea3fa526bcc238a621bd
Rafael <rafa_sena@msn.com>
2021-08-20 08:59:14 -0300

    main

---
+++ layout/cabecalho.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>001_laboratorio</title>
+    <link rel="stylesheet" href="css/estilo.css" type="text/css"/>
+</head>
+<body>
+    <div class="central">
+        <div class="cabecalho">
+            <img src="#" alt="aqui terá uma img"/>
+        </div>
+        <div class="principal">
